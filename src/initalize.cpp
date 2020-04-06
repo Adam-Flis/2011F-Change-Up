d3ce5bfef4de37a3277bd4987a41b92157978ebd
Adam F <59700364+Jelly2011@users.noreply.github.com>
2020-04-05 22:27:39 -0400

    Added namespace pros, 3 src files

---
+++ src/initalize.cpp
@@ -0,0 +1,16 @@
+#include "main.h"
+
+//Code that runs when you start the program
+void initialize(){
+
+}
+
+//Code that runs when the robot is in the disabled state by the competition switch or the field controller
+void disabled(){
+
+}
+
+//Code that runs after initalize when connected to a competition switch or field controller
+void competition_initialize(){
+
+}
---
+++ src/opcontrol.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+
+void opcontrol(){
+  while(1){
+
+    
+    delay(10); //Loop speed, prevent overload
+  }
+}
